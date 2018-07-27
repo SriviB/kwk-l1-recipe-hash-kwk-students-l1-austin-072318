99086d3183d22bc6c9ef0f9be9cadb9b6e87583b
SriviB <srivibalaji33@gmail.com>
2018-07-27 17:14:56 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -14,14 +14,14 @@
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
 perfect_10_recipe = {
-  :almonds_flour => "2 cups",
-  :gluten_free_whole_oats => "1 cup",
-  :kosher_salt => "1/2 tea spoon",
-  :baking_powder => "1/2 tea spoon",
-  :baking_soda => "1/4 tea spoon",
-  :xanthan_gum => "1/2 tea spoon",
-  :slivered_almonds_lightly_toasted => "1/4 cup",
-  :mini_dark_chocolate_chips => "3/4 cup",
+      :almonds_flour => "2 cups",
+      :gluten_free_whole_oats => "1 cup",
+      :kosher_salt => "1/2 tea spoon",
+      :baking_powder => "1/2 tea spoon",
+      :baking_soda => "1/4 tea spoon",
+      :xanthan_gum => "1/2 tea spoon",
+      :slivered_almonds_lightly_toasted => "1/4 cup",
+      :mini_dark_chocolate_chips => "3/4 cup",
   :olive_oil => "1/2 cups"
 }
 
